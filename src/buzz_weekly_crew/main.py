bdfd9abe2d04b250059b002a1e52a2cc1f6fd654
Johan Uddståhl <baktakt@gmail.com>
2024-09-15 19:10:40 +0000

    Improvements to tasks and agents instructions

--- src/buzz_weekly_crew/main.py
+++ src/buzz_weekly_crew/main.py
@@ -7,6 +7,10 @@ def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
         "rss-feeds": os.getenv('RSS_FEEDS'),
+        "number-of-articles-from-each-feed": os.getenv('ARTICLES_PER_FEED'),
+        "number-of-articles-in-blog-post": os.getenv('ARTICLES_IN_BLOG'),
+        "topics": os.getenv('TOPICS'),
+        "blog-name": os.getenv('BLOG_NAME'),
               }
     BuzzWeeklyCrew().crew().kickoff(inputs=inputs)
 
@@ -17,6 +21,10 @@ def train():
     """
     inputs = {
         "rss-feeds": os.getenv('RSS_FEEDS'),
+        "number-of-articles-from-each-feed": os.getenv('ARTICLES_PER_FEED'),
+        "number-of-articles-in-blog-post": os.getenv('ARTICLES_IN_BLOG'),
+        "topics": os.getenv('TOPICS'),
+        "blog-name": os.getenv('BLOG_NAME'),
               }
     try:
         BuzzWeeklyCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
